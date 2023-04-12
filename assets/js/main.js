4122ce4c20eb1b850b388d0140e9f5085fb3ae9c
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2023-04-12 19:22:20 +0400

    fix slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,8 @@
+//slide card
 const featuresMain = document.querySelector('#features-main');
 let isDown = false;
 let startX;
 let scrollLeft;
-featuresMain.addEventListener('touchstart', (event) => {
-    isDown = true;
-    startX = event.touches[0].pageX - featuresMain.offsetLeft;
-    scrollLeft = featuresMain.scrollLeft;
-  });
-
-  featuresMain.addEventListener('touchend', () => {
-    isDown = false;
-  });
   featuresMain.addEventListener('touchmove', (e) => {
     if (!isDown) return;
     e.preventDefault();
